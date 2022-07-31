7b33de9722e883d060c91f11f8afa1245fd2dd6d
Karol Stajer <kastajer1@gmail.com>
2022-08-01 00:11:05 +0200

    flip animation

--- src/Pages/Learn.js
+++ src/Pages/Learn.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { useState } from 'react'
+import anime from 'animejs/lib/anime.es.js';
 
 const Learn = ({data}) => {
 
     const [index, setIndex] = useState(0);
-    const [showReverse, setShowReverse] = useState(false);
 
-    
+    const animation = ()  => {
+
+        index < data.length-1 ? setIndex(index+1) : console.log("")
+    }
 
   return (
     <div className='center learn'>
@@ -15,16 +18,22 @@ const Learn = ({data}) => {
                 {"<"}
             </div>
             <div className='middle-column'>
-                <div className={showReverse ? 'card cardback' : 'card'} onClick={() => {setShowReverse(!showReverse)}}>
-                    {showReverse ? data[index].definition : data[index].term}
-                    
+                <div className='card'>
+                    <div className="flip-card-inner">
+                        <div className="flip-card-front">
+                            <p>{data[index].term}</p>
+                        </div>
+                        <div className="flip-card-back">
+                            <p>{data[index].definition}</p>
+                        </div>
+                    </div> 
                 </div>
                 <div className='center progress'>
                     {index+1}/{data.length} flashcards
                 </div>
             </div>
 
-            <div className='arrow' onClick={() => {index < data.length-1 ? setIndex(index+1) : console.log("")}}>
+            <div className='arrow' id='leftarrow' onClick={animation}>
                 {">"}
             </div>
         </div>
